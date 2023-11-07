3eaab230683971157961b1c98bef59d3786bcb3e
David Caro <david.caro.dec@hotmail.com>
2023-11-07 20:27:27 -0300

    feature/CORS-configuration-JWT-Baerer-token-Authentication-and-mailer-integration

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,14 +4,29 @@ import { AppService } from './app.service';
 import { SocialmediaController } from './socialmedia/socialmedia.controller';
 import { SocialmediaService } from './socialmedia/socialmedia.service';
 import { ConfigModule } from '@nestjs/config';
+import { MailerController } from './mailer/mailer.controller';
+import { MailerService } from './mailer/mailer.service';
+import { JwtStrategy } from './auth/strategies/jwt.strategy';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
     }),
+    JwtModule.register({
+      secret: process.env.JWT_SECRET_KEY,
+      signOptions: { expiresIn: '60s' },
+      
+    })
   ],
-  controllers: [AppController, SocialmediaController],
-  providers: [AppService, SocialmediaService],
+  controllers: [AppController, SocialmediaController, MailerController],
+  providers: [
+    AppService, 
+    SocialmediaService, 
+    MailerService, 
+    JwtStrategy
+  ],
 })
+
 export class AppModule {}
