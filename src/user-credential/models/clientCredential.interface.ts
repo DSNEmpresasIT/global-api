2d9a15b79d880b760b35e619bed93d2b346c3cd6
Daniel M <danielmino926@gmail.com>
2023-11-15 20:04:05 -0300

    change the interface structure into sub-interfaces

--- src/user-credential/models/clientCredential.interface.ts
+++ src/user-credential/models/clientCredential.interface.ts
@@ -2,26 +2,32 @@ export interface ClientCredential {
   clientName: string;
   supabaseUrl?: string;
   supabaseKey?: string;
-  facebook?: {
-    tokenId: string;
-    pageId: string;
-  };
+  facebook?: Facebook;
   instagram?: string;
   recapcha?: ReCaptchaKeys;
-  email?: {
-    host: string;
-    user: string;
-    password: string;
-    port: string;
-    emailJs: {
-      publicKey: string;
-      service: string;
-      contactTemplate: string;
-    };
-  };
+  email?: Email;
+}
+
+export interface Facebook {
+  tokenId: string;
+  pageId: string;
 }
 
 export interface ReCaptchaKeys {
   key: string;
   secretKey: string;
 } 
+
+export interface Email {
+  host: string;
+  user: string;
+  password: string;
+  port:string;
+  emailJS?: EmailJs;
+}
+
+export interface EmailJs {
+  publicKey: string;
+  service: string;
+  contactTemplate: string;
+}
