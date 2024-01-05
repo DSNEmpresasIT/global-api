b25521c5581e7e0f74e02794d8f8b20a9f90966f
David Caro <david.caro.dec@hotmail.com>
2024-01-05 19:00:54 -0300

    COMMITEA POR FAVOR

--- src/projects/projects.controller.ts
+++ src/projects/projects.controller.ts
@@ -13,7 +13,7 @@ export class ProjectsController {
     return this.service.createProject(param.clientId, body)
   }
  
-  @Get(':clientId/client')
+  @Get('client/:clientName')
   async getClientProjects(@Param() params) {
     const clientId = params.clientId;
     return await this.service.getAllClientProjects(clientId);
