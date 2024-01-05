c74e82c77ca69749765de2ffcdaa4d4be96f7e61
David Caro <david.caro.dec@hotmail.com>
2024-01-05 19:03:07 -0300

    merge

--- src/projects/projects.controller.ts
+++ src/projects/projects.controller.ts
@@ -13,7 +13,7 @@ export class ProjectsController {
     return this.service.createProject(param.clientId, body)
   }
  
-  @Get('client/:clientName/client')
+  @Get('client/:clientName')
   async getClientProjects(@Param() params) {
     const clientId = params.clientId;
     return await this.service.getAllClientProjects(clientId);
