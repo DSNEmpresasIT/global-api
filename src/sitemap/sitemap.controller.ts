07ef6d2893b3a183f1d2dac55a13ef4a6cacf222
David Caro <david.caro.dec@hotmail.com>
2024-01-29 10:30:13 -0300

    Roles and JWT authentication

--- src/sitemap/sitemap.controller.ts
+++ src/sitemap/sitemap.controller.ts
@@ -1,6 +1,10 @@
-import { Body, Controller, Get, Param, Query } from '@nestjs/common';
+import { Body, Controller, Get, Param, Query, UseGuards } from '@nestjs/common';
 import { SitemapService } from './sitemap.service';
 import { GetSiteMapDto, QueryParamsGetSitemap } from './dto/sitemap.dto';
+import { JwtGuard } from 'src/auth/guards/jwt.guard';
+import { RoleGuard } from 'src/auth/guards/role.guard';
+import { Roles } from 'src/auth/decorators/roles.decorator';
+import { RolesTypes } from 'src/auth/decorators/roles.interface';
 
 @Controller('api/sitemap')
 export class SitemapController {
@@ -8,6 +12,8 @@ export class SitemapController {
     private readonly service: SitemapService,
   ) {}
 
+  @Roles(RolesTypes.ADMIN)
+  @UseGuards(JwtGuard, RoleGuard)
   @Get(':clientId')
   async getSitemap(@Param() param, @Query() query: QueryParamsGetSitemap, @Body() body: GetSiteMapDto) {
     return this.service.getSitemap(param.clientId, query, body);
